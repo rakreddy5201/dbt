015a2ef77c799d9370fb3ca48ad90fb4c42e88cf
rak r <rakreddy5201@gmail.com>
2025-09-10 11:59:42 +0000

    great expectetation package added

--- models/example/regions.sql
+++ models/example/regions.sql
@@ -6,5 +6,6 @@ country_name,
 case when country_code in ('US','CA') then 'North America'
 when country_code in ('GB') then 'Europe'
 when country_code in ('IN') then 'Asia' else null end as region,
+to_date(current_date()) as created_at,
 {{ dbt_utils.generate_surrogate_key(['country_name', 'region']) }} as sk
 from {{ ref('country_codes') }}
