5024270df358598d859fb552f92bd7ce3cd64499
rak r <rakreddy5201@gmail.com>
2025-09-10 09:59:36 +0000

    added packages

--- models/example/regions.sql
+++ models/example/regions.sql
@@ -5,5 +5,6 @@ select
 country_name,
 case when country_code in ('US','CA') then 'North America'
 when country_code in ('GB') then 'Europe'
-when country_code in ('IN') then 'Asia' else null end as region
+when country_code in ('IN') then 'Asia' else null end as region,
+{{ dbt_utils.generate_surrogate_key(['country_name', 'region']) }} as sk
 from {{ ref('country_codes') }}
